48ab3deda0452890953854b34e32a9a7ade3db23
kreative358 <105795822+kreative358@users.noreply.github.com>
2024-09-16 22:41:45 +0200

    production 19

--- docker/production/django/start.sh
+++ docker/production/django/start.sh
@@ -5,12 +5,13 @@ set -o errexit
 set -o pipefail
 
 set -o nounset
-python /app/manage.py runserver 0.0.0.0:8004
+
 python /app/manage.py collectstatic --noinput
+python /app/manage.py migrate users
 python /app/manage.py migrate
 
-NUM_WORKERS=${GUNICORN_WORKERS:-3}
-# NUM_WORKERS=${3:-3}
+# NUM_WORKERS=${GUNICORN_WORKERS:-3}
+NUM_WORKERS=${3:-3}
 
 exec /usr/local/bin/gunicorn config.wsgi --bind 0.0.0.0:8004 --chdir=/app --workers $NUM_WORKERS
 # exec /usr/local/bin/gunicorn config.wsgi --bind 0.0.0.0:8000 --chdir=/app --workers $NUM_WORKERS
